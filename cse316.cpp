dde12d258e312e5472e895ca8faf4b9a014c6367
Nandasagarre <62168236+Nandasagarre@users.noreply.github.com>
2020-03-28 17:04:45 +0530

    Update cse316.cpp

--- cse316.cpp
+++ cse316.cpp
@@ -1,129 +1,148 @@
 #include<stdio.h>
 #include<conio.h>
+#include<stdlib.h>
 using namespace std;
 
 /* the variables used : 
     n=number of procresses, time= instanteneous time unit, req= resource requriments(n)
-	p[]=procresses, at[]=arrival time, bt[]=brust time, wait[]=waiting quaque, ubt[]=updated brust time           */
+	p[]=procresses, at[]=arrival time, bt[]=brust time, wait[]=waiting quaque, pri[]=priority queue,com[]= completed procresses
+	****************by default the procresses are a,b,c,d,e,f,g,h,i,j*********which are pre loaded into r[] list        */
 int main()
-{  int n,req,i=0,q=0,max=0;
-   int p[10], wait[10], at[10], bt[10], ubt[10],que[10];
+{  int n,i,k,j=1;
+   int at[10], bt[10], pri[15],CT[10],TAT[10],WT[10],com[10];           // arrival time;brust time;,priorty queue;
+   char r[10];                             //ready queue
+   char p[11]={'a','b','c','d','e','f','g','h','i','j','\0'}; //procresses list
+   char wait[15];                                  //waitlist
 
-
+// user enters the procresses, its respective arrival and brust times here
 printf("Number of procresses=");
 scanf("%d",&n);
-req=n;
+int z=n;   //internal computation purpose
+if(n>10)   //manages exceptions
+{ printf("out of capacity\n");
+  printf("minimum 10 is allowed;\ntry again");
+exit(0);
+}
 
-	 for(i=0; i<n;i++)                  // complexity O(n) for loop
+
+	 for(i=0; i<n;i++)                  // ------------complexity O(n) for loop---
 	{
-		printf("Enter Arrival Time and Burst Time For Process p: %d:",i+1);
-		scanf("%d",&at[i]);         // array at[0,5,13,16] arraival time
-		scanf("%d",&bt[i]);         // array bt[20,36,19,42]brust times of respective procresses
-		                
+		printf("Enter Arrival Time For Process %c:",p[i]);
+		scanf("%d",&at[i]);            
+	                 		                
 	}
-	
-	
-// first case scheduler alocating the cpu resources with quantum time 6 units	
-q=6;
-printf("quantum time siez= 6 units\n\n");
-for(i=0; i< n;i++)            // complexity O(n)+O(n)=O(n)
-{ if(bt[i]>=q)
-   {ubt[i]=bt[i]-q;}                /* updating the brust time list of the procresses after the 6 units of resource alloaction
-                                        and storing the results into ubt[] array*/    									                                    
-}
-//updating waitlist(uncomplete procresses) stored in wait[] array
 for(i=0;i<n;i++)
-{ if(ubt[i]!=0)
-{ wait[i]=1;}
-  else
-  { wait[i]=0;
-  }
+{ 	  printf("Enter Brust Time For Process %c:",p[i]);
+      scanf("%d",&bt[i]); 
+	wait[i]=p[i];
+//loading all avalble procresses into wait list intially;
 }
 
-for(i=0;i<n;i++)
-                          // complexity O(n)
-{  printf("updated brust time procress p%d :",i+1);       // updated brust time
-   printf("%d\n",ubt[i]); 
+
+/*since in the given problem it was metioned that user u'd be allowed to enter the arrival and brust times only
+hence ***it is assumed that the assigning of priorities is totally the programmer call;
+and ******also the problem statement demands for the computation of waiting and turn around time*/
+
+//assigning the procresses with priority;
+for(i=0;i<n;i++)  //complexity O(n);
+{ pri[i]=i+3;
+  printf("The priority of procress:   %c%d\n",wait[i],pri[i]); 
 }
-  printf("note if waitlist 1 means recpective procress is incomplete\n");
-   printf("note if waitlist 0 means recpective procress is completed\n");
- for(i=0;i<n;i++)
-{
-  printf("updated wait list procress p%d :",i+1) ;
- 
-   printf("%d\n",wait[i]);
-   }
-   
-   
-
-// second case sheduler allocating the cpu resources with quantum time 10;
-q=10;
-printf("quantum time siez= 10 units\n\n");
-for(i=0; i< n;i++)            // complexity O(n)+O(n)=O(n)
-{ if(bt[i]>=q)
-   {ubt[i]=bt[i]-q;}                /* updating the brust time list of the procresses after the 6 units of resource alloaction
-                                        and storing the results into ubt[] array*/    									                                    
-}
-//updating waitlist(uncomplete procresses) stored in wait[] array
-for(i=0;i<n;i++)
-{ if(ubt[i]!=0)
-{ wait[i]=1;}
-  else
-  { wait[i]=0;
-  
-  }
-}
-
-for(i=0;i<n;i++)
-                          // complexity O(n)
-{  printf("updated brust time procress p%d :",i+1);       // updated brust time
-   printf("%d\n",ubt[i]); 
-}
-  printf("note if waitlist 1 means recpective procress is incomplete\n");
-   printf("note if waitlist 0 means recpective procress is completed\n");
- for(i=0;i<n;i++)
-{
-  printf("updated wait list procress p%d :",i+1) ;
-  
-   printf("%d\n",wait[i]);
-   }
-   
-// third case shedhuler with variable qunatum siez depending on the max brust time;
-printf("variable qunatum time siez depends on maximum brust time");
-printf("by slecting the procress that req max time to run\n");
-
-
-
-
-for(i=0;i<n;i++)
+/*for(i=0;i<n;i++)
 { 
+printf("wait list initially%d",wait[i]);
+}
+for(i=9;i<=0;i--)
+{ 
+printf("updated wait list after assignf priorities%d",wait[i]);
+}*/
+CT[0]=bt[0];
+TAT[0]=0;
+WT[0]=0;
+//the calculation of waiting time and timearound time
+for(i=0;i<n;i++)
+{ CT[i+1]=CT[i]+bt[i+1];    //completion time;
+  TAT[i]=CT[i]-at[i];
+  j++;
+  if(TAT[i]<0)        //expection handlings
+  {printf("you have not entered proper values\n");
+   printf("some of the values are turning into negtive wich is not accpetable;");
+   printf("try again with proper set of values");
+   exit(0);
+  } 
+   else 
+   {
 
-for(int j=0;j<n;j++)
-{  
-     if(ubt[j]>ubt[j+1])    //findin the process which requries max time; 
-      { max=j+1;          //**** if equal brust times by default the first one i.e according to arrival time is chhosen; 
-      }
-}   
-
-
-    if(max!=0) 
-	{
-		
-      	printf("allocating cpu resources for p%d for %d units of brust time\n",max,ubt[max-1]);
-      	ubt[max-1]=0;
-      	
-	}
-	else
-	{printf("completed");
-	}
+  WT[i]=TAT[i]-bt[i];   //wait time; 
 }
 }
 
+printf("--------------------------------------------summary------------------------------------------------------------\n\n");
+printf("                AT      BT      CT     TAT(ct-at)   WT(tat-bt) of\n");	
+for(i=n-1;i>=0;i--)
+{ 
+//printf("The turnaround time of %c is %d units\n",wait[i],TAT[i]);
+//printf("The wait time of %c is %d unitd\n",wait[i],WT[i]);
+printf("procress:  %c   %d |   %d   |    %d  |      %d     |       %d\n",wait[i],at[i],bt[i],CT[i],TAT[i],WT[i]);}
+
+
+printf("importat note:\nhere the quantum time is equal brut time.\n the procress is can take its required time to run to its completion\n");
+printf("The priority is given to the process in a unusual way by the programer\n");
+printf("****notice the sequence of exuction of the procresses under summmary;\nits according to the priority");
+
+}
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+   
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 
